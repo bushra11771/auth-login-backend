11891b75257d4e378db262ed648608c2f899879d
bushra11771 <bushra11771@gmail.com>
2025-07-23 20:13:31 +0500

    fix package

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverlessExpress = require('@vendia/serverless-express');
-const app = require('../server'); // Import your express app
+const app = require('../server');
 
 module.exports = serverlessExpress({ app });
